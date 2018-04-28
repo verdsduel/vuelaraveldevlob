4252b70e4faf301d6cfaa45bbc96f563c9e9ffef
v3rds <verdadero.matt@gmail.com>
2018-04-28 08:56:45 +0800

    Intro to Components

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="header clearfix">
+		<nav>
+			<ul class="nav nav-pills pull-right">
+				<li role="presentation" class="active">
+					<a href="#">Home</a>
+				</li>
+				<li role="presentation">
+					<a href="#">About</a>
+				</li>
+				<li role="presentation">
+					<a href="#">Contact</a>
+				</li>
+			</ul>
+		</nav>
+		<h3 class="text-muted">
+			Project Name
+		</h3>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	
+</style>
