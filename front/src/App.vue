0a27321d44f816b31ef06d2a6cf490ba3e4affb9
v3rds <verdadero.matt@gmail.com>
2018-04-28 09:56:46 +0800

    Authentication

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="container" id="app">
     <my-navbar></my-navbar>
+    <login></login>
+    <register></register>
   </div>
 </template>
 
 <script>
   
   import Navbar from './components/Navbar.vue'
+  import Login from './components/authentication/Login.vue'
+  import Register from './components/authentication/Register.vue'
 
   export default {
     components: {
-      'my-navbar': Navbar
+      'my-navbar': Navbar,
+      'login'    : Login,
+      'register'    : Register
     }
   }
 </script>
